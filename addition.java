7c56d6fc39373af17e1721e0d8776d53f679a176
23701A04D4 <lharithareddy13@gamil.com>
2025-08-19 12:02:11 +0530

    addition program

---
+++ addition.java
@@ -0,0 +1,20 @@
+package haritha;
+
+public class addition {
+    public static void main(String[] args) {
+		int a,b,c,d,f,e,h;//TO DEFINE DATATYPE INTERGER AND VARIBLES A,B,C
+		a=10;b=5;
+		c=a+b;//addition
+		d=a-b;//sub
+		e=a*b;//mul
+		f=a/b;//div
+		System.out.println("enter the values c is addition:"+c);// add function to mension the variable C
+		System.out.println("enter the values d is sub :"+d);
+		System.out.println("enter the values e is multiplication:"+e);
+		System.out.println("enter the values f is division :"+f);
+	}
+// TODO Auto-generated method stub
+
+	}
+
+
